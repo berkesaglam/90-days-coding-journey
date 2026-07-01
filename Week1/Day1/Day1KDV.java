c349589d166c7602a6c25592f7eee28b80730539
Berke Sağlam <168022537+berkesaglam@users.noreply.github.com>
2026-07-01 23:56:55 +0300

    Refactor Day1NotOrtalaması to Day1KDVHesabı
    Updated the class to calculate KDV from product cost instead of grades.

--- Week1/Day1/Day1KDV.java
+++ Week1/Day1/Day1KDV.java
@@ -1,36 +1,27 @@
-package day1notortalaması;
+package day1kdvhesabı;
 import java.util.Scanner;
-
 /**
  *
  * @author berke
  */
-public class Day1NotOrtalaması {
+public class Day1KDVHesabı {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        //vize %40, final %40, quiz %20
-        //60 üstü geçer
+        /*
+        ürün maliyeti al
+        kdv oranını çıkarıp gerçek maliyetini göster
+        */
         Scanner scan = new Scanner(System.in);
-        System.out.print("Vize notunuzu giriniz: ");
-        int vizegrade = scan.nextInt();
-        System.out.print("Quiz notunuzu giriniz: ");
-        int quizgrade = scan.nextInt();
-        System.out.print("Final notunuzu girniz: ");
-        int finalgrade = scan.nextInt();
+        double kdvoranı = 0.18;
+        System.out.print("Ürününüzün maliyetini girin: ");
+        double fiyat = scan.nextDouble();
         
-        double ortalama = vizegrade / 10 * 4 + quizgrade / 10 * 2 + finalgrade / 10 * 4;
-        
-        System.out.println("Vize: " + vizegrade);
-        System.out.println("Quiz: " + quizgrade);
-        System.out.println("Final: " + finalgrade);
-        System.out.println("Ortalama: " + ortalama);
-        
-        String kontrol = (ortalama >= 60) ? "Geçti" : "Kaldı";
-        System.out.println("Geçme durumunuz: " + kontrol);
+        double maliyet =fiyat - ( fiyat * kdvoranı);
+        System.out.println("KDVsiz ürün fiyatı: " + maliyet);
         scan.close();
     }
     
